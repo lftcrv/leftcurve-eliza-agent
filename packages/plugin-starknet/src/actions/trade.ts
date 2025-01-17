98cad27544a0a9a4e3c652ea5a09f7c0c1c396a9
sBaki92 <sbaki92120@gmail.com>
2025-01-17 15:06:49 +0100

    add-sending-some-trade-infos

--- packages/plugin-starknet/src/actions/trade.ts
+++ packages/plugin-starknet/src/actions/trade.ts
@@ -23,9 +23,14 @@ import {
 } from "@avnu/avnu-sdk";
 
 import { RpcProvider, Contract } from "starknet";
-
 import { getStarknetAccount } from "../utils/index.ts";
 import { validateStarknetConfig } from "../environment.ts";
+import * as dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
+
+const BACKEND_API_KEY = process.env.BACKEND_API_KEY;
 
 interface MarketData {
     currentPrice: number;
@@ -86,25 +91,37 @@ interface TradeDecision {
 }
 
 const tokens = [
-    { address: "0x03b405a98c9e795d427fe82cdeeeed803f221b52471e3a757574a2b4180793ee", name: "BROTHER" },
-    { address: "0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac", name: "BTC" },
-    { address: "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7", name: "ETH" },
-    { address: "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d", name: "STRK" },
-    { address: "0x0124aeb495b947201f5fac96fd1138e326ad86195b98df6dec9009158a533b49", name: "LORDS" },
-    { address: "0x68f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8", name: "USDT" },
-    { address: "0x53c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8", name: "USDC" },
-    { address: "0x42b8f0484674ca266ac5d08e4ac6a3fe65bd3129795def2dca5c34ecc5f96d2", name: "wstETH" },
-    { address: "0x3fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac", name: "WBTC" },
-    { address: "0x49210ffc442172463f3177147c1aeaa36c51d152c1b0630f2364c300d4f48ee", name: "UNI" },
-    { address: "0x5574eb6b8789a91466f902c380d978e472db68170ff82a5b650b95a58ddf4ad", name: "DAI" },
-    { address: "0x319111a5037cbec2b3e638cc34a3474e2d2608299f3e62866e9cc683208c610", name: "rETH" },
-    { address: "0x70a76fd48ca0ef910631754d77dd822147fe98a569b826ec85e3c33fde586ac", name: "LUSD" },
-    { address: "0x28d709c875c0ceac3dce7065bec5328186dc89fe254527084d1689910954b0a", name: "xSTRK" },
-    { address: "0xc530f2c0aa4c16a0806365b0898499fba372e5df7a7172dc6fe9ba777e8007", name: "NSTR" },
-    { address: "0x585c32b625999e6e5e78645ff8df7a9001cf5cf3eb6b80ccdd16cb64bd3a34", name: "ZEND" },
-    { address: "0x4878d1148318a31829523ee9c6a5ee563af6cd87f90a30809e5b0d27db8a9b", name: "SWAY" },
-    { address: "0x102d5e124c51b936ee87302e0f938165aec96fb6c2027ae7f3a5ed46c77573b", name: "SSTR" },
+    { address: "0x3b405a98c9e795d427fe82cdeeeed803f221b52471e3a757574a2b4180793ee", name: "BROTHER", decimals: 18 },
+    { address: "0x3fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac", name: "BTC", decimals: 8 },
+    { address: "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7", name: "ETH", decimals: 18 },
+    { address: "0x4718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d", name: "STRK", decimals: 18 },
+    { address: "0x124aeb495b947201f5fac96fd1138e326ad86195b98df6dec9009158a533b49", name: "LORDS", decimals: 18 },
+    { address: "0x68f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8", name: "USDT", decimals: 6 },
+    { address: "0x53c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8", name: "USDC", decimals: 6},
+    { address: "0x42b8f0484674ca266ac5d08e4ac6a3fe65bd3129795def2dca5c34ecc5f96d2", name: "wstETH", decimals: 18},
+    { address: "0x3fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac", name: "WBTC", decimals: 8},
+    { address: "0x49210ffc442172463f3177147c1aeaa36c51d152c1b0630f2364c300d4f48ee", name: "UNI", decimals: 18},
+    { address: "0x5574eb6b8789a91466f902c380d978e472db68170ff82a5b650b95a58ddf4ad", name: "DAI", decimals: 18},
+    { address: "0x319111a5037cbec2b3e638cc34a3474e2d2608299f3e62866e9cc683208c610", name: "rETH", decimals:18 },
+    { address: "0x70a76fd48ca0ef910631754d77dd822147fe98a569b826ec85e3c33fde586ac", name: "LUSD", decimals: 18},
+    { address: "0x28d709c875c0ceac3dce7065bec5328186dc89fe254527084d1689910954b0a", name: "xSTRK", decimals: 18},
+    { address: "0xc530f2c0aa4c16a0806365b0898499fba372e5df7a7172dc6fe9ba777e8007", name: "NSTR", decimals: 18},
+    { address: "0x585c32b625999e6e5e78645ff8df7a9001cf5cf3eb6b80ccdd16cb64bd3a34", name: "ZEND", decimals: 18},
+    { address: "0x4878d1148318a31829523ee9c6a5ee563af6cd87f90a30809e5b0d27db8a9b", name: "SWAY", decimals: 6},
+    { address: "0x102d5e124c51b936ee87302e0f938165aec96fb6c2027ae7f3a5ed46c77573b", name: "SSTR", decimals: 18},
   ];
+
+function convertSellAmount(swap: Swap): number | null {
+  const token = tokens.find(t => t.address.toLowerCase() === swap.sellTokenAddress.toLowerCase());
+  if (!token) {
+    console.error("Token not found for address:", swap.sellTokenAddress);
+    return null;
+  }
+  const decimals = token.decimals;
+  const sellAmount = swap.sellAmount;
+  const result = Number(sellAmount) / 10 ** decimals;
+  return result;
+}
 
 const fetchTokenDetails = async (tokenAddress: string): Promise<TokenDetails> => {
     const url = `https://starknet.impulse.avnu.fi/v1/tokens/${tokenAddress}`;
@@ -292,9 +309,7 @@ export const tradeAction: Action = {
 
 {{{bio}}}
 
-Based on the market data, wallet information and some last news, decide if it's interesting to make a swap.
-
-Warning: To avoid fee issues, always ensure you have at least 0.0016 ETH and 12 STRK.
+Based on the market data, your wallet balances and some last news, decide if it's interesting to make a swap.
 
 ⚠️ **Strict Response Format (JSON only):**
 Do not add any extra text before or after the JSON block. Follow the structure exactly.
@@ -336,8 +351,10 @@ Do not add any extra text before or after the JSON block. Follow the structure e
 - Do not add any extra explanation or text.
 - Ensure JSON syntax is correct (commas, quotes, etc.).
 
+Warning: To avoid fee issues, always ensure you have at least 0.0016 ETH and 12 STRK.
+
 \n\n
-{{{providers}}}` + CurrentNews + "\n\n And \n\n" + tokenInfos + "\n\n And \n\n" +tokenPrices;
+` + CurrentNews + "\n\n And \n\n" + tokenInfos + "\n\n And \n\n" +tokenPrices + "{{{providers}}}";
 ;
 
         const shouldTradeContext = composeContext({
@@ -405,6 +422,23 @@ Do not add any extra text before or after the JSON block. Follow the structure e
                             "Swap completed successfully! tx: " +
                             swapResult.transactionHash,
                     });
+
+                    const sellTokenName = tokens.find(t => t.address.toLowerCase() === swap.sellTokenAddress.toLowerCase()).name;
+                    const buyTokenName = tokens.find(t => t.address.toLowerCase() === swap.buyTokenAddress.toLowerCase()).name;
+                    const message =
+                    `{"agentId": ${state.agentId}, "tradeId": ${swapResult.transactionHash},
+                    trade:
+                     {"sellTokenName": ${sellTokenName}, "sellTokenAddress" : ${swap.sellTokenAddress},
+                     "buyTokenName": ${buyTokenName}, "buyTokenAddress" : ${swap.buyTokenAddress},
+                     "sellAmount": ${convertSellAmount(swap)}, "buyAmount": ${quote[0].buyAmount}
+                    }}`;
+
+                    console.log(message);
+                    axios.post('http://localhost:5000', { message: 'Hello from TS!' }, {
+                      headers: { 'Content-Type': 'application/json', 'API_KEY_VALUE': BACKEND_API_KEY }
+                    }).then(res => console.log(res.data))
+                      .catch(err => console.error(err));
+
                     return true;
                 } catch (error) {
                     console.log("Error during token swap:", error);
