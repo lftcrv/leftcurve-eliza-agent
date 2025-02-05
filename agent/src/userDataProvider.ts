922ea553f65c58ad54141f0f8be8ebcf54071ac9
julienbrs <julien.bourseau@gmail.com>
2025-02-06 00:36:14 +0100

    ⚰️ Remove dead code

--- agent/src/userDataProvider.ts
+++
@@ -1,9 +0,0 @@
-import { IAgentRuntime, Memory, Provider } from "@ai16z/eliza";
-
-const userDataProvider: Provider = {
-    get: async (runtime: IAgentRuntime, message: Memory) => {
-        return "SOME USER DATA !!!!!";
-    },
-};
-
-export {userDataProvider};
--- prisma/migrations/20250123100316_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "wallet" JSONB NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
