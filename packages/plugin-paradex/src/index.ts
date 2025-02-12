ff452740563b8c340552e506dac476cc81e2486c
julienbrs <julien.bourseau@gmail.com>
2025-02-12 12:08:45 +0100

    🔥 remove recent messages in managewatchlist

--- packages/plugin-paradex/src/index.ts
+++ packages/plugin-paradex/src/index.ts
@@ -4,13 +4,13 @@ import { manageWatchlistAction } from "./actions/manageWatchlist";
 import { getWatchlistAction } from "./actions/getWatchlist";
 import { bboProvider } from "./providers/bbo";
 import { watchlistProvider } from "./providers/watchlist";
-import { paradexAuthAction } from "./actions/authentification";
+import { paradexFetchAccountInfoAction } from "./actions/fetchAccountInfo";
 import { paradexPlaceOrderAction } from "./actions/placeOrder";
 
 export const paradexPlugin: Plugin = {
     name: "paradex",
     description: "Paradex Plugin for Eliza",
-    actions: [manageWatchlistAction, paradexAuthAction, paradexPlaceOrderAction],
+    actions: [manageWatchlistAction, paradexFetchAccountInfoAction, paradexPlaceOrderAction],
     providers: [watchlistProvider],
 };
 
